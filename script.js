e63b7c8fc9129a58f431cb55a90ec5632a1bd7d8
Arjun <arjunvaradiyil203@gmail.com>
2024-08-21 21:54:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.write("hello ");
+document.write("<br>");
+document.write("gggg");
+console.warn("error")
+console.log("this is test msg")
+console.error("error")
+
+
+//let var const
+
+let person
+person="arjun"
+
+var age=19
+console.log(age)
+
+
+const pi=3.14
+console.log(pi)
+
